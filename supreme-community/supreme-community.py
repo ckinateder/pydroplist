026cbfc7377ba7f78525c8ed6a7aee3b94046641
Calvin Kinateder <calvinkinateder1@yahoo.com>
2020-11-05 15:08:58 -0500

    added new files

---
+++ supreme-community/supreme-community.py
@@ -0,0 +1,22 @@
+# AUTHOUR: CALVIN KINATEDER
+import requests, json, bs4
+
+def latest():
+    base_url = 'https://www.supremecommunity.com/'
+    weeks_ext = 'season/fall-winter2020/droplists/'
+    weeks_list = requests.get(base_url+weeks_ext)
+    soup = bs4.BeautifulSoup(weeks_list.content, 'html.parser')    
+    latest_url = base_url+soup.find("div", {"id": "box-latest"}).a['href']
+
+    drop_page = requests.get(latest_url)
+    drop_soup = bs4.BeautifulSoup(drop_page.content, 'html.parser')    
+    droppy = drop_soup.find_all('h2', 'name item-details item-details-title')
+
+    droplist = list()
+
+    for i in droppy:
+        droplist.append(i.text)
+    
+    return droplist
+
+print(latest())
