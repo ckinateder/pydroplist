72aa5edec27283d29b2e6dbcc273ea048e0f73d1
Calvin Kinateder <calvinkinateder1@yahoo.com>
2020-11-05 18:21:09 -0500

    add gitignore

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="supreme-community", # Replace with your own username
+    name="supreme-community-ckinateder", # Replace with your own username
     version="0.0.1",
     author="Calvin Kinateder",
     author_email="calvinkinateder@gmail.com",
     description="A python module to grab data from supremecommunity.com and list drop products",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ckinateder/supreme-community",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU GPLv3 License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
